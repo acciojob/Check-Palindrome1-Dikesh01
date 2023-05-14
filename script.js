d2c1c9ef04b31b27dabcca0d9a18973280c111da
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-14 18:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	let is_palindrome = true;
+	let i=0, j=str.length-1;
+	while(i<=j){
+		if(str.charAt(i) !=str.charAt(j)){
+			is_palindrome= false;
+			break;
+		}
+		i++;
+		j--;
+	}
+	is_palindrome ? console.log("palindrome")  : console.log("not a palindrome")
+	
 }
 module.exports = palindrome
