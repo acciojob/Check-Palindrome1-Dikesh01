ff82c38f3f4324640bdf8ad7bc20808d01510eb4
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-14 18:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ function palindrome(str){
 	let is_palindrome = true;
 	let i=0, j=str.length-1;
 	while(i<=j){
-		if(str.charAt(i) !=str.charAt(j)){
+		if(str[i]  != str[j]){
 			is_palindrome= false;
 			break;
 		}
 		i++;
 		j--;
 	}
-	is_palindrome ? console.log("palindrome")  : console.log("not a palindrome")
-	
+console.log(is_palindrome ? "Palindrome" : "Not a Palindrome");
 }
 module.exports = palindrome
